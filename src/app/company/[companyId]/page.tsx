2a39186d2495880fb93c2c37a50e5424264b8ab6
HRNPH <hrnph@protonmail.com>
2024-03-26 18:45:13 +0700

    feat: dumb typing but working detail page

--- src/app/company/[companyId]/page.tsx
+++ src/app/company/[companyId]/page.tsx
@@ -95,11 +95,17 @@ const mockJobs: Job[] = [
 
 async function getCompany(companyId: string): Promise<Job> {
   const company = await fetch(`/api/company/${companyId}`);
-  return company.json();
+  const companyData = await company.json() as {
+    success: boolean;
+    data: Job;
+  }
+  return companyData.data as Job;
 }
 
 export default function Home({ params }: { params: { companyId: string } }) {
-  const company = mockJobs.find((job) => job._id === params.companyId);
+  // const company = mockJobs.find((job) => job._id === params.companyId);
+  // const company = getCompany(params.companyId);
+  const [company, setCompany] = useState<Job | null>(null);
   const [selectedDate, setSelectedDate] = useState(null);
 
   const handleDateChange = (date: any) => {
@@ -107,51 +113,70 @@ export default function Home({ params }: { params: { companyId: string } }) {
   }
 
   useEffect(() => {
-    getCompany(params.companyId);
+    const fetchData = async () => {
+      const a_company = await getCompany(params.companyId);
+      setCompany(a_company ?? null);
+      console.log(
+        "a fucnklign company",
+        a_company,
+      );
+    };
+    fetchData();
   }, []);
 
   return (
     <div className="text-black bg-white h-[100vh]">
       <div className="py-[40px]"></div>
-      <div className="border-2 border-gray-300 rounded-2xl w-[70%] mx-auto">
-        <div className="flex justify-around mx-auto p-5 py-[50px]">
-          <div>
-            <div className="mb-2">
-              <Link href={'/explore'} className="mb-[20px] text-[20px]">
-                <FontAwesomeIcon icon={faArrowLeftLong} className="fas fa-check" style={{ color: "black" }}
-                ></FontAwesomeIcon> ย้อนกลับ
-              </Link>
-            </div>
-            <img className="rounded" src={company?.image} alt="company image" width={600} height={800} />
-          </div>
+      {
+        company ? (
+          <div className="border-2 border-gray-300 rounded-2xl w-[70%] mx-auto">
 
-          <div>
-            <div className="my-4 font-semibold text-3xl">{company?.name}</div>
-            <div>
-              <div className="mt-2 font-semibold">รายละเอียดบริษัท</div>
-              <div className="font-medium text-[18px] w-[200px]">{company?.description}</div>
-            </div>
-            <div>
-              <div className="font-semibold mt-4">วันที่ต้องการจอง</div>
+            <div className="flex justify-around mx-auto p-5 py-[50px]">
               <div>
-                <DatePicker
-                  selected={selectedDate}
-                  onChange={(date: any) => setSelectedDate(date)}
-                  showTimeSelect
-                  timeFormat="HH:mm"
-                  timeIntervals={15}
-                  dateFormat="MMMM d, yyyy h:mm aa"
-                  timeCaption="Time"
-                  className="border-2 border-gray-300 rounded-md"
-                />
+                <div className="mb-2">
+                  <Link href={'/explore'} className="mb-[20px] text-[20px]">
+                    <FontAwesomeIcon icon={faArrowLeftLong} className="fas fa-check" style={{ color: "black" }}
+                    ></FontAwesomeIcon> ย้อนกลับ
+                  </Link>
+                </div>
+                <img className="rounded" src={company?.image} alt="company image" width={600} height={800} />
+              </div>
+
+              <div>
+                <div className="my-4 font-semibold text-3xl">{company?.name}</div>
+                <div>
+                  <div className="mt-2 font-semibold">รายละเอียดบริษัท</div>
+                  <div className="font-medium text-[18px] w-[200px]">{company?.description}</div>
+                </div>
+                <div>
+                  <div className="font-semibold mt-4">วันที่ต้องการจอง</div>
+                  <div>
+                    <DatePicker
+                      selected={selectedDate}
+                      onChange={(date: any) => setSelectedDate(date)}
+                      showTimeSelect
+                      timeFormat="HH:mm"
+                      timeIntervals={15}
+                      dateFormat="MMMM d, yyyy h:mm aa"
+                      timeCaption="Time"
+                      className="border-2 border-gray-300 rounded-md"
+                    />
+                  </div>
+                </div>
+                <button className="mt-5 flex items-center justify-center bg-primary text-white px-[1rem] text-[18px] rounded-[10px] h-[55px] border-2 border-transparent hover:bg-cutoff-white hover:border-primary hover:text-primary flex-none">
+                  ยืนยันการจอง
+                </button>
               </div>
             </div>
-            <button className="mt-5 flex items-center justify-center bg-primary text-white px-[1rem] text-[18px] rounded-[10px] h-[55px] border-2 border-transparent hover:bg-cutoff-white hover:border-primary hover:text-primary flex-none">
-              ยืนยันการจอง
-            </button>
           </div>
-        </div>
-      </div>
+
+        ) : (
+          // spinner
+          <div className="flex justify-center items-center h-[100px]">
+            <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-primary"></div>
+          </div>
+        )
+      }
     </div>
   );
 }
