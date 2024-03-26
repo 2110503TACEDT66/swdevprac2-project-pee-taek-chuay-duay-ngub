3fae05c5561d685473ed6c838f118cf8a0553438
K1EW <weerawatwmt@gmail.com>
2024-03-26 19:58:19 +0700

    mookrob

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -5,7 +5,6 @@ import { mockUser, mockCompany } from "@/mock_data/mocks";
 export default function Home({ params }: { params: { id: string } }) {
   const user = mockUser.find((user) => user._id === params.id);
   const company = mockCompany.find((company) => company._id === user?.company);
-  console.log("User company ID", user?.company);
 
   return (
     <div className="bg-white h-[100vh] text-black">
