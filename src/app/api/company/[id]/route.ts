c158370e344e452fa0b9fd1c2d2f8926a75e83bb
HRNPH <hrnph@protonmail.com>
2024-03-26 18:37:17 +0700

    fix: working single conmapany request routes

--- src/app/api/company/[id]/route.ts
+++ src/app/api/company/[id]/route.ts
@@ -4,14 +4,19 @@ export async function GET(
     request: Request,
     { params }: { params: { id: string } }
 ) {
-    const id = params.id // 'a', 'b', or 'c'
-    console.log('GET Company by ID:', id)
+    try {
 
-    const response = await callInternAPIById(
-        InternApiRoutes.GetCompanyById,
-        'GET',
-        id
-    )
-    console.log('Company:', response)
-    return Response.json(response ?? { error: 'Company not found' })
+        const id = params.id // 'a', 'b', or 'c'
+        console.log('GET Company by ID:', id)
+
+        const response = await callInternAPIById(
+            InternApiRoutes.GetCompanyById,
+            'GET',
+            id
+        )
+        console.log('Company:', response)
+        return Response.json(response ?? { error: 'Company not found' })
+    } catch (error:any) {
+        return Response.json({ error: error.message }, { status: 500 })
+    }
 }
